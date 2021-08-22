76ca23932785b551efde055aa9213e6489ea337c
tohzen <melvin.l.mobley@gmail.com>
2021-08-21 22:35:35 -0400

    updated

--- anime-project-hooks-frontend/src/App.js
+++ anime-project-hooks-frontend/src/App.js
@@ -1,16 +1,12 @@
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import axios from "axios"
-import React, {useState, useEffect} from "react";
 
 import Spinner from "./components/Spinner/Spinner";
 import MainRouter from "./MainRouter";
 
 import AuthContextWrapper from "./context/AuthContext";
 
-
-
 function App() {
-  
   return (
     <React.Suspense fallback={<Spinner />}>
       <Router>
