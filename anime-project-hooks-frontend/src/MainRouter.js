802de2c83a1886577e00e4cfb63c6ea5ac576156
tohzen <melvin.l.mobley@gmail.com>
2021-08-21 22:43:04 -0400

    updated

--- anime-project-hooks-frontend/src/MainRouter.js
+++ anime-project-hooks-frontend/src/MainRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 // import Auth from "./components/Auth/Auth";
 // import Home from "./components/Home/Home";
 // import NotFound from "./components/NotFound/NotFound";
@@ -9,7 +10,8 @@ import Navbar from "./components/Navbar/Navbar";
 const Home = React.lazy(() => import("./components/Home/Home"));
 const Auth = React.lazy(() => import("./components/Auth/Auth"));
 const NotFound = React.lazy(() => import("./components/NotFound/Notfound"));
-
+const Protected = React.lazy(() => import("./components/Protected/Protected"));
+const Profile = React.lazy(() => import("./components/Profile/Profile"));
 
 function MainRouter() {
   return (
@@ -20,6 +22,9 @@ function MainRouter() {
         <Route exact path="/login" component={Auth} />
         <Route exact path="/logout" render={() => <Redirect to="/login" />} />
         <Route exact path="/" component={Home} />
+
+        <PrivateRoute exact path="/protected" component={Protected} />
+        <PrivateRoute exact path="/profile" component={Profile} />
 
         <Route component={NotFound} />
       </Switch>
