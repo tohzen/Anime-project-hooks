f824bc817671c075ca0ed14b4d0b27f816262a81
tohzen <melvin.l.mobley@gmail.com>
2021-08-21 22:20:01 -0400

    updated

--- anime-project-hooks-backend/routes/users/usersRouter.js
+++ anime-project-hooks-backend/routes/users/usersRouter.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var passport = require("passport");
 var { createUser, login } = require("./controller/userController");
 
 /* GET users listing. */
@@ -9,5 +10,18 @@ router.get("/", function (req, res, next) {
 
 router.post("/create-user", createUser);
 router.post("/login", login);
+
+router.put(
+  "/update-profile",
+  passport.authenticate("jwt-user", { session: false }),
+  function (req, res) {
+    res.send("YAY!!!");
+  }
+);
+
+router.get("/logout", function (req, res) {
+    res.clearCookie("jwt-cookie");
+    res.send("Logged out!");
+});
 
 module.exports = router;
