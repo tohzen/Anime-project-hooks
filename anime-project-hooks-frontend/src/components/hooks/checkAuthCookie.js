0a830041727c48fa70c33ba642f2ed59cb67ea15
tohzen <melvin.l.mobley@gmail.com>
2021-08-21 22:29:06 -0400

    updated

--- anime-project-hooks-frontend/src/components/hooks/checkAuthCookie.js
+++ anime-project-hooks-frontend/src/components/hooks/checkAuthCookie.js
@@ -1,44 +1,42 @@
 import { useContext } from "react";
 import Cookie from "js-cookie";
 import jwtDecode from "jwt-decode";
-import { AuthContext} from "../../context/AuthContext";
 
-function CheckAuthCookie(){
-    const { dispatch } = useContext(AuthContext);
+import { AuthContext } from "../../context/AuthContext";
+function CheckAuthCookie() {
+  const { dispatch } = useContext(AuthContext);
 
-    function checkIfCookieExists(){
-        const cookie = Cookie.get("jwt-cookie");
+  function checkIfCookieExists() {
+    const cookie = Cookie.get("jwt-cookie");
 
-        if(cookie){
-            return true;
-        }else{
-            return false;
-        }
-
-        
+    if (cookie) {
+      return true;
+    } else {
+      return false;
     }
-    function logUserIn(){
-        
+  }
 
-        if(checkIfCookieExists()){
-            const cookie = Cookie.get("jwt-cookie");
-            const jwtDecodedToken = jwtDecode(cookie);
+  function logUserIn() {
+    let checkCookieExists = checkIfCookieExists();
 
-            dispatch({
-                type: "LOGIN",
-                user: {
-                    email: jwtDecodedToken.email,
-                    username: jwtDecodedToken.username,
-                },
-            });
-        }
+    if (checkCookieExists) {
+      const cookie = Cookie.get("jwt-cookie");
+      const jwtDecodedToken = jwtDecode(cookie);
+
+      dispatch({
+        type: "LOGIN",
+        user: {
+          email: jwtDecodedToken.email,
+          username: jwtDecodedToken.username,
+        },
+      });
     }
-    return{
-        checkIfCookieExists, 
-        logUserIn,
-    }
+  }
+
+  return {
+    checkIfCookieExists,
+    logUserIn,
+  };
 }
-
-
 
 export default CheckAuthCookie;
